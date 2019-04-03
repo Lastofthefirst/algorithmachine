22d608f9901517d3122d5f476ad471f89166bf14
Lastofthefirst <47003973+Lastofthefirst@users.noreply.github.com>
2019-04-03 11:13:30 -0400

    Solar Calc

--- docs/js/main.js
+++ docs/js/main.js
@@ -368,3 +368,101 @@ function walls(height){
   let wallArea = circumference * height;
   return wallArea;
 }
+
+
+// Solar Panel code
+
+/*
+"use strict";
+
+function addMonths(elem){
+  let annualUseKw = 0;
+  let dailyUseKw = 0;
+
+  let i = 0;
+  let x = 0;
+
+  var months = document.getElementById(elem).getElementsByTagName('input');
+
+    for(i=0; i<months.length; i++) {
+        x = Number(months[i].value);
+          annualUseKw += x;
+    }
+  dailyUseKw = annualUseKw/365;
+  return dailyUseKw;
+}
+
+var dailyUseKw = addMonths('mpc');
+jslint browser:true */
+
+
+function dailyKwh(){
+  let monthlyUse = document.getElementById('kwhIn').value;
+  return monthlyUse * 12/365;
+}
+
+function sunHours(){
+let hrs;
+let theZone = document.getElementById('zoneIn').selectedIndex;
+theZone +=1;
+switch(theZone) {
+  case 1:
+    hrs = 6;
+    break;
+    console.log(hrs);
+  case 2:
+    hrs = 5.5;
+  case 3:
+    hrs = 5;
+    break;
+  case 4:
+    hrs = 4.5;
+    break;
+  case 5:
+    hrs = 4.2;
+    break;
+  case 6:
+    hrs = 3.5;
+    break;
+  default:
+    hrs = 0;
+}
+  console.log(hrs);
+  return hrs;
+
+}
+function calculatePanel(){
+let userChoice = document.getElementById('panelIn').selectedIndex;
+let panelOptions = document.getElementById('panelIn').options;
+let power = panelOptions[userChoice].value;
+let name = panelOptions[userChoice].text;
+let x = [power, name]
+return x;
+}
+
+function calculateSolar(){
+  let dailyUseKw = dailyKwh();
+  let sunHoursPerDay = sunHours();
+
+  let minKwNeeds = dailyUseKw/sunHoursPerDay;
+  let realKwNeeds = minKwNeeds * 1.25;
+
+  let realWattNeeds = realKwNeeds * 1000;
+
+  let panelInfo = calculatePanel();
+  let panelOutput = panelInfo[0];
+  let panelName = panelInfo[1];
+
+  let panelsNeeded = Math.ceil(realWattNeeds/panelOutput);
+
+  let feedback = "";
+    feedback += "<p>Based on your average daily use of " + Math.round(dailyUseKw) + "kwh, you will need "+ panelsNeeded+" "+ panelName+" solar panels to offset 100% of your average daily electrical consumption.</p>";
+    feedback += "<h2>Additional Details</h2>";
+    feedback += "<p>Your average daily electicity consuption: "+ Math.round(dailyUseKw) +" Kwh per day.</p>"
+    feedback += "<p>Average sunshine hours per day: "+ sunHoursPerDay+" hours.</p>"
+    feedback += "<p>Realistic watts needed per hour: "+ Math.round(realWattNeeds)+" watts/hour.</p>"
+    feedback += "<p>The "+panelName+" panel you selected generates about "+ panelOutput+" watts per hour.</p>"
+
+    document.getElementById('solarRecommendation').innerHTML = feedback;
+
+}
