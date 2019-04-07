b55d0dd007b3e8d5c85430dc22e48ccf97b6556d
Lastofthefirst <47003973+Lastofthefirst@users.noreply.github.com>
2019-04-07 15:51:59 -0400

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -17,28 +17,10 @@ var app  = new Framework7({
     };
   },
   // App root methods
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
-    },
-  },
-  // App routes
   routes: routes,
 });
 
 // Init/Create main view
 var mainView = app.views.create('.view-main', {
   url: '/'
-});
-
-// Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
-
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
-
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
 });
